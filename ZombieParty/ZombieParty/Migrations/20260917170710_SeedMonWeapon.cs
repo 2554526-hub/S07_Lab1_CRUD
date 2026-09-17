45e5c2a198262d4dbb2d9c207cefa9c682d4af02
Mathias Kelway-Ramirez <2554526@cegepmontpetit.ca>
2026-09-17 13:08:42 -0400

    FCT Seed Zombie ZombieType et Weapon
    Création données de départ pour Zombie, ZombieType et Weapon

---
+++ ZombieParty/ZombieParty/Migrations/20260917170710_SeedMonWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZombieParty.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMonWeapon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Weapons",
+                columns: new[] { "WeaponId", "CreatedDate", "Description", "Force", "Image", "Name", "Price", "Qty", "QtyBought" },
+                values: new object[] { 3, new DateTime(1999, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "An awesome Long Steel Sword", 150m, "https://www.pngkey.com/maxpic/u2y3a9r5q8u2e6w7/#google_vignette", "Long Sword", 340m, 17, 12 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Weapons",
+                keyColumn: "WeaponId",
+                keyValue: 3);
+        }
+    }
+}
